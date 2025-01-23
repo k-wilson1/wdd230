b554bd9257acbfac955794296b5f8c3ec3a558e5
k-wilson1 <katsouttathebag@outlook.com>
2025-01-23 17:56:47 -0600

    hamburger button

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-    hamButton.addEventListener('click', () => {
-        navigation.classList.toggle('hide');
-        hamButton.classList.toggle('open');
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('hide');
+    hamButton.classList.toggle('open');
 });    

3f77432afda9286a9e7c3470890155fb5ad7f71d
k-wilson1 <katsouttathebag@outlook.com>
2025-02-08 12:32:03 -0600

    nav and added weather

---
+++ chamber/scripts/join.js
@@ -0,0 +1,11 @@
+const button= document.querySelector('button');
+button.textContent = 'Join Us!';
+button.id= 'join';
+button.className= 'join';
+
+
+button.addEventListener('click', () => {
+    alert('Welcome to the Chamber of Commerce! We look forward to working with you.');
+});
+
+document.body.appendChild(button);

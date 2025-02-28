630a4bee3beec253eaba3ba44782bb8713720b3f
k-wilson1 <katsouttathebag@outlook.com>
2025-02-27 22:15:35 -0600

    form page

---
+++ scripts/form.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        let formt = event.target;
+        let formData = new FormData(formt);
+
+        for (let pair of formData.entries()) {
+            console.log(pair[0] + ":" + pair[1]);
+        }
+    }
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
+});
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+};
+
+const pw1 = document.querySelector("#password");
+const pw2 = document.querySelector("#password2");
+
+pw2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (pw1.value !== pw2.value) {
+        message.textContent = "Passwords do not match";
+        message.style.visibility = "show";
+        pw2.style.backgroundColor = "fff0f3";
+        pw2.value = "";
+        pw2.focus();
+    }
+    else {
+        message.style.display = "none";
+        pw2.style.backgroundColor = "#fff";
+        pw2.style.color = "#000";
+    }
+};

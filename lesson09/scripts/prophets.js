5b772309b86cad3da3d6c4299585f7c97297bb44
k-wilson1 <katsouttathebag@outlook.com>
2025-03-04 20:47:48 -0600

    prophets activity

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -5,6 +5,31 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        cards.appendChild(card);
+    });
 }
 getProphetData();

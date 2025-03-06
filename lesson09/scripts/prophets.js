4d0b536fec57d371424733719c03c33fe7df1d61
k-wilson1 <katsouttathebag@outlook.com>
2025-03-05 18:29:09 -0600

    prophet cards

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -12,23 +12,26 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
         let birthDate = document.createElement('p');
         let birthPlace = document.createElement('p');
+        let portrait = document.createElement('img');
+
 
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
-        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        
 
         card.appendChild(fullName);
-        card.appendChild(portrait);
         card.appendChild(birthDate);
         card.appendChild(birthPlace);
+        card.appendChild(portrait);
+        
         cards.appendChild(card);
     });
 }

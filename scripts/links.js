1a88580232205a660c88dcc95a71708a8226be80
k-wilson1 <katsouttathebag@outlook.com>
2025-03-06 21:53:52 -0600

    links JSON

--- scripts/links.js
+++ scripts/links.js
@@ -1,31 +1,32 @@
 const baseURL = "https://k-wilson1.github.io/wdd230/";
 
 const linksURL = "https://k-wilson1.github.io/wdd230/data/links.json";
+const cards = document.querySelector('#cards');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     if (response.ok) {
         const data = await response.json();
-        console.log(data);
-        displayLinks(data); 
+        displayLinks(data.lessons); 
     }
 }
 const displayLinks = (links) => {
     links.forEach((link) => {
-        let cards = document.createElement('section');
         let linkUrl = document.createElement('a');
-        let title = document.createElement('p');
-        let heading = document.createElement('h2');
+        let title = document.createElement('li');
+        let lesson = document.createElement('ul');
+       
         lesson.textContent = `${link.lesson}`;
         title.textContent = `${link.title}`;
         linkUrl.textContent = `${link.link}`;
-        heading.textContent = `Learning Activities`;
 
-        cards.appendChild(heading);
+
         cards.appendChild(lesson);
         cards.appendChild(title);    
         cards.appendChild(linkUrl);
+
+
     }
-        )
-}
+        );
+};
 getLinks();

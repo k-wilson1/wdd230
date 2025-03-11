84c9fb217545298b8e6566e3933fdcffd0ad39e1
k-wilson1 <katsouttathebag@outlook.com>
2025-03-11 18:13:36 -0500

    chamber fixes

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,25 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=8b6fb4ffd212493a899438d7792a8ad2&units=imperial";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data); 
+        } else {
+            throw Error(await response.text());
+        } 
+    }catch (error) {
+            console.log(error);
+        }
+    }
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather.description}.04n`
+}
+apiFetch();

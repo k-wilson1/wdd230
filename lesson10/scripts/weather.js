a6011abfd5bc169070b2b95e3042307c38791be4
k-wilson1 <katsouttathebag@outlook.com>
2025-03-13 20:00:17 -0500

    weather API

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,6 +1,6 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("#figcaption");
+const captionDesc = document.querySelector("#desc");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=8b6fb4ffd212493a899438d7792a8ad2&units=imperial";
 
@@ -23,7 +23,7 @@ function displayResults(data) {
     const iconCode = data.weather[0].icon;
     const iconsrc = `https://openweathermap.org/img/w/${iconCode}.png`;
     
-    let desc = data.weather[0].description;
+    const desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc.charAt(0).toUpperCase() + desc.slice(1)}`;

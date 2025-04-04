4bd913e168adbc71d0508aaa46dd4d6489e0d783
k-wilson1 <katsouttathebag@outlook.com>
2025-04-04 12:50:26 -0500

    added image to my rental for cozumel project

--- cozumel/scripts/rentals.js
+++ cozumel/scripts/rentals.js
@@ -5,7 +5,6 @@ async function getRentals() {
     const response = await fetch(rentalsURL);
     if (response.ok) {
         const data = await response.json();
-        console.log(rentalsURL);
         displayRentals(data.rentals);
     }
 }
@@ -15,22 +14,30 @@ const displayRentals = (rentals) => {
         let card = document.createElement('section');
         let type = document.createElement('h2');
         let persons = document.createElement('p');
-        let reservations = document.createElement('h1');
+        let img =  document.createElement('img');
+        let reservations = document.createElement('h3');
         let reservationhalfday = document.createElement('p');
         let reservationfullday = document.createElement('p');
-        let walkin = document.createElement('h1');
+        let walkin = document.createElement('h3');
         let walkinhalfday = document.createElement('p');
         let walkinfullday = document.createElement('p');
 
         type.textContent = rental.type;
         persons.textContent = `Max Persons: ${rental.persons}`;
+        img.setAttribute('src', rental.image);
+        img.setAttribute('alt', rental.type);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute('width', '100');
+        img.setAttribute('height', '100');
         reservations.textContent = "Reservations";
-        reservationhalfday.textContent = `Half Day: ${rental.reservationhalfday}`;
-        reservationfullday.textContent = `Full Day: ${rental.reservationfullday}`;
+        reservationhalfday.textContent = `Half Day: ${rental.reservations[0].halfDay}`;
+        reservationfullday.textContent = `Full Day: ${rental.reservations[0].fullDay}`;
+    
         walkin.textContent = "Walk-in";
-        walkinhalfday.textContent = `Half Day: ${rental.walkinhalfday}`;
-        walkinfullday.textContent = `Full Day: ${rental.walkinfullday}`;
+        walkinhalfday.textContent = `Half Day: ${rental.walkin[0].halfDay}`;
+        walkinfullday.textContent = `Full Day: ${rental.walkin[0].fullDay}`;
         card.appendChild(type);
+        card.appendChild(img);
         card.appendChild(persons);
         card.appendChild(reservations);
         card.appendChild(reservationhalfday);
